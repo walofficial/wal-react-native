1e348ad80d6cad318afd97bc2ceee7cdd75f76e0
nshelia <nshprimary@gmail.com>
2025-09-01 01:59:00 +0400

    refactor: remove duplicate import in SpacesBottomSheet component

--- components/SpacesBottomSheet/index.tsx
+++ components/SpacesBottomSheet/index.tsx
@@ -1,5 +1,4 @@
 import React, { RefObject, useCallback } from 'react';
-import React, { useCallback } from 'react';
 import { BottomSheetFooter, BottomSheetModal } from '@gorhom/bottom-sheet';
 import BottomSheet, { BottomSheetBackdrop } from '@gorhom/bottom-sheet';
 import SpacesSheetHeader from './SpacesSheetHeader';
