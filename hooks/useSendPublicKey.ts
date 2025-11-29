357a24816b8dc510921a267e9b6d83b9f79bd373
nshelia <nshprimary@gmail.com>
2025-11-29 20:12:04 +0400

    chore: remove unused test files and workspace configuration
    - Deleted ThemedText test file and its associated snapshot to clean up the test suite.
    - Removed ProtocolService test file to streamline the codebase.
    - Eliminated workspace configuration file for Android to reduce clutter in the project.

--- hooks/useSendPublicKey.ts
+++ hooks/useSendPublicKey.ts
@@ -22,8 +22,8 @@ export default function useSendPublicKey() {
     }) => {
       const { identityKeyPair, isCached } =
         await ProtocolService.generateIdentityKeyPair();
-      console.log('identityKeyPair', identityKeyPair);
-      if (isCached) {
+
+        if (isCached) {
         // Avoid resetting the timestamp of the public key and setting new public key
         setPublicKey(identityKeyPair.publicKey);
         return;
